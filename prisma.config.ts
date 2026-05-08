1c1fb57183eee10fa35bd77cb7a53d219e545c75
mwebesa lawrence <lawrencemwebesa28@gmail.com>
2026-05-08 17:50:25 +0300

    third commit

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   earlyAccess: true,
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+  datasourceUrl: process.env.DATABASE_URL!,
   migrate: {
     async development() {
       return {
